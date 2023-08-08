f51cb9c965751b963d1f5c121eaaf80e0713964f
Cheleste1994 <wolverines@tut.by>
2023-08-08 19:31:36 +0300

    feat: add search and login

---
+++ src/routes/Registration/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Counter from '../../components/partials/Home/Counter';
+
+const Login = () => (
+  <div className="login">
+    <Counter />
+  </div>
+);
+
+export default Login;
